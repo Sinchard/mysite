fc6235e000e4b8539a2d645a13149ffa7acb89e2
Sinchard Smith <a52581120@163.com>
2020-01-15 15:49:32 +0800

    add index view to test

--- reader/views.py
+++ reader/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.template import loader
 
 # Create your views here.
+def index(request):
+    template = loader.get_template('reader/index.html')
+    context = {
+        'title': "My Web",
+    }
+    return HttpResponse(template.render(context, request))
