92f97f62a09ce358ab529edebb17906134752c27
Sinchard Smith <a52581120@163.com>
2020-01-15 15:51:17 +0800

    add fiction,chapter and content model

--- reader/models.py
+++ reader/models.py
@@ -1,3 +1,34 @@
 from django.db import models
 
+
 # Create your models here.
+class BaseModel(models.Model):
+    name = models.CharField(max_length=200, null=False)
+    url = models.CharField(max_length=200, null=False)
+
+    class Meta:
+        abstract = True
+
+
+class Fiction(BaseModel):
+    fiction_id = models.PositiveIntegerField(null=False)
+
+    def __str__(self):
+        return "fiction name:{0},fiction url:{1}".format(self.name, self.url)
+
+
+class Chapter(BaseModel):
+    fiction_id = models.PositiveIntegerField(null=False)
+    chapter_id = models.DecimalField(max_digits=20, decimal_places=2, null=False)
+
+    def __str__(self):
+        return "chapter name:{0},fiction url:{1}".format(self.name, self.url)
+
+
+class Content(models.Model):
+    fiction_id = models.PositiveIntegerField(null=False)
+    chapter_id = models.DecimalField(max_digits=20, decimal_places=2, null=False)
+    content = models.TextField()
+
+    def __str__(self):
+        return "Content:{0}".format(self.content)
