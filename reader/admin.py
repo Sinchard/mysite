318488f8f5c1e0ca99c2481a9644fb21dce738c8
Sinchard Smith <a52581120@163.com>
2020-01-18 23:09:44 +0800

    add and customize the models to the administration site

--- reader/admin.py
+++ reader/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from .models import Fiction, Chapter, Content
 
-# Register your models here.
+class FictionAdmin(admin.ModelAdmin):
+    list_display = ('fiction_id', 'url', 'name', 'updated')
+    list_filter = ('updated',)
+    search_fields = ('fiction_id', 'name')
+    date_hierarchy = 'updated'
+    ordering = ['fiction_id']
+admin.site.register(Fiction, FictionAdmin)
+
+admin.site.register(Chapter)
+admin.site.register(Content)
