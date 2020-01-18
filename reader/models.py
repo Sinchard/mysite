5a612ed500cc7f8ff246ab91d06dd82e44d46aa0
Sinchard Smith <a52581120@163.com>
2020-01-18 22:58:33 +0800

    add update time to Fiction, Chapter and Content

--- reader/models.py
+++ reader/models.py
@@ -5,6 +5,7 @@ from django.db import models
 class BaseModel(models.Model):
     name = models.CharField(max_length=200, null=False)
     url = models.CharField(max_length=200, null=False)
+    updated = models.DateTimeField(auto_now=True)
 
     class Meta:
         abstract = True
@@ -30,6 +31,7 @@ class Content(models.Model):
     fiction_id = models.PositiveIntegerField(null=False, db_index=True)
     chapter_id = models.DecimalField(max_digits=20, decimal_places=2, null=False, db_index=True)
     content = models.TextField()
+    updated = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return "Content:{0}".format(self.content)
