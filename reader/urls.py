99a2ffeef79a0302443fb4c0ddf740991b53914f
Sinchard Smith <a52581120@163.com>
2020-01-19 20:54:39 +0800

    add chapter and content url path

--- reader/urls.py
+++ reader/urls.py
@@ -1,7 +1,11 @@
 from django.urls import path
+from django.conf.urls import url
 
 from . import views
 
 urlpatterns = [
     path(r'', views.index, name='index'),
+    url(r'^(?P<fid>\d+)/(?P<cid>\d+)/',views.getContent,name='content'),
+    url(r'^(?P<fid>\d+)/',views.getChapter,name='chapter'),
+
 ]
