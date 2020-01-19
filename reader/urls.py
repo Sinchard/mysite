5ca3c943ac680a93ec2b0b0fa12ba2f429d715e4
Sinchard Smith <a52581120@163.com>
2020-01-19 21:48:38 +0800

    change url for index ListView class

--- reader/urls.py
+++ reader/urls.py
@@ -6,6 +6,6 @@ from . import views
 urlpatterns = [
     path(r'', views.index, name='index'),
     url(r'^(?P<fid>\d+)/(?P<cid>\d+)/',views.getContent,name='content'),
-    url(r'^(?P<fid>\d+)/',views.getChapter,name='chapter'),
-
+    #url(r'^(?P<fid>\d+)/',views.getChapter,name='chapter'),
+    url(r'^$', views.PostListView.as_view(), name='post_list'),
 ]
