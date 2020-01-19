3c9b806d8603ae07470456ed3f289292c6b62f7a
Sinchard Smith <a52581120@163.com>
2020-01-19 20:55:47 +0800

    add chapter and content view, add fiction view paginator

--- reader/views.py
+++ reader/views.py
@@ -1,11 +1,34 @@
 from django.shortcuts import render
-from django.http import HttpResponse
-from django.template import loader
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from .models import Fiction, Chapter, Content
+
 
 # Create your views here.
 def index(request):
-    template = loader.get_template('reader/index.html')
-    context = {
-        'title': "My Web",
-    }
-    return HttpResponse(template.render(context, request))
+    print('fiction view')
+    fictions = Fiction.objects.all()
+    paginator = Paginator(fictions, 50)  # 3 posts in each page
+    page = request.GET.get('page')
+    try:
+        posts = paginator.page(page)
+    except PageNotAnInteger:
+        # If page is not an integer deliver the first page
+        posts = paginator.page(1)
+    except EmptyPage:
+        # If page is out of range deliver last page of results
+        posts = paginator.page(paginator.num_pages)
+    return render(request, 'reader/index.html', {'fictions': fictions, 'page': page, 'title': '小说列表'})
+
+
+def getChapter(request, fid):
+    print('chapter view')
+    fiction = Fiction.objects.filter(fiction_id=fid)[0]
+    chapters = Chapter.objects.filter(fiction_id=fid).order_by("chapter_id")
+    return render(request, 'reader/chapter.html', {'fiction': fiction, 'chapters': chapters})
+
+
+def getContent(request, fid, cid):
+    print('content view')
+    chapter = Chapter.objects.filter(fiction_id=fid, chapter_id=cid)[0]
+    content = Content.objects.filter(fiction_id=fid, chapter_id=cid)[0]
+    return render(request, 'reader/content.html', {'chapter': chapter, 'content': content})
